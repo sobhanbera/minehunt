d13e6e0e606567ab63d24eb273389d923d77874d
sobhanbera <sobhanbera258@gmail.com>
2021-12-11 19:35:29 +0530

    internationalization initiated with following initial languages
    English, Hindi, Bengali

---
+++ app/i18n/index.js
@@ -0,0 +1,64 @@
+/**
+ * © 2010 Sobyte
+ *
+ * @author : Sobhan Bera (sobhanbera)
+ * @other_editors :
+ * @file : Javascript
+ *
+ * Purpose - application's main i18n controller service
+ */
+
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import locale from 'react-native-locale-detector';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import en from './en.json'; /*  english          */
+import hi from './hi.json'; /*  hindi            */
+import bn from './bn.json'; /*  bengali          */
+// import other from './other.json' /*  other          */
+
+import {LANGUAGE_CODE_STORAGE_KEY} from '../constants';
+
+/**
+ * locale system language detector
+ */
+const languageDetector = {
+    init: Function.prototype,
+    type: 'languageDetector',
+    async: true, // flags below detection to be async
+    detect: async callback => {
+        const savedDataJSON = await AsyncStorage.getItem(
+            LANGUAGE_CODE_STORAGE_KEY,
+        );
+        const lng = savedDataJSON ? savedDataJSON : null;
+        const selectLanguage = lng || locale;
+        callback(selectLanguage);
+    },
+    cacheUserLanguage: () => {},
+};
+
+i18n.use(languageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        react: {
+            useSuspense: false,
+        },
+        resources: {en /* english */, hi /* hindi */, bn /* bengali */},
+
+        ns: ['common'],
+        defaultNS: 'common',
+
+        /**
+         * this config is needed or the following error will be encountered
+         * i18next::pluralResolver: Your environment seems not to be Intl API compatible, use an Intl.PluralRules polyfill.
+         */
+        compatibilityJSON: 'v3',
+
+        interpolation: {
+            escapeValue: false, // not needed for react as it does escape per default to prevent xss!
+        },
+    });
+
+export default i18n;
