d7e47da079b367eb37c730821befd17a881a0472
sobhanbera <sobhanbera258@gmail.com>
2021-12-13 01:07:57 +0530

    game screen initial layout

--- app/screens/GameScreen.tsx
+++ app/screens/GameScreen.tsx
@@ -1,10 +1,194 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useEffect, useState} from 'react';
+import {View, Animated, PanResponder, StatusBar} from 'react-native';
 
-export default function GameScreen() {
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+// import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
+
+import {Header} from '../components';
+import {useTheme} from '../contexts';
+import {DEVICE_STATUSBAR_HEIGHT_CONSTANT, MEDIUM_ICON_SIZE} from '../constants';
+import {GameData} from '../interfaces';
+import {formatSecondsToTimer} from '../utils';
+
+// const {Value} = Animated;
+
+interface Props {
+    navigation: {
+        navigate: Function;
+    };
+    route: {
+        params: {
+            gameData: GameData;
+        };
+    };
+}
+const GameScreen = (props: Props) => {
+    const {themeColors} = useTheme();
+
+    const {route, navigation} = props;
+    const {gameData} = route.params;
+    // console.log(props.route.params.gameData);
+
+    /**
+     * pan object containing a 2d coordinate
+     */
+    const pan = useState(new Animated.ValueXY())[0];
+    /**
+     * the actual pan responder object
+     * this is used to perform actions like to move the pan value or update it
+     * or when the pan release action is triggered
+     * or else
+     */
+    const panResponder = useState(
+        PanResponder.create({
+            onMoveShouldSetPanResponder: () => true,
+            onPanResponderGrant: () => {
+                pan.setOffset({
+                    x: pan.x._value,
+                    y: pan.y._value,
+                });
+            },
+            onPanResponderMove: (_, gesture) => {
+                // if(maximumPanMoveX(gameData.rows, gameData.columns) <= gesture.dx) {}
+                pan.x.setValue(gesture.dx);
+                pan.y.setValue(gesture.dy);
+            },
+            onPanResponderRelease: () => {
+                pan.flattenOffset();
+            },
+        }),
+    )[0];
+    // console.log(pan.getLayout(), pan.getTranslateTransform());
+
+    const [timer, setTimer] = useState<number>(0);
+
+    useEffect(() => {
+        let timer = setInterval(() => {
+            setTimer(value => value + 1);
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    /**
+     * function to start the game for the first time
+     * this may be triggered when the initial cell is minned
+     */
+    // function startGame() {}
+
+    /**
+     * when the user presses the restart button
+     * this function will restart the game
+     */
+    // function restartGame() {}
+
+    /**
+     * minned on a cell containing mine in it
+     * so this function will control all actions of game over
+     */
+    // function gameOver() {}
+
+    /**
+     * main game renderer
+     */
     return (
-        <View>
-            <Text>Game</Text>
+        <View
+            style={{
+                flex: 1,
+                // backgroundColor: 'red',
+            }}>
+            <StatusBar translucent={true} />
+
+            {/**
+             * an extra view to show at the statusbar
+             * when rendered inside header compoenent many other changes are needed
+             * so used an extra view for that purpose
+             * 1. also this contains a transparent background color for a elegent look
+             **/}
+            <View
+                style={{
+                    height: DEVICE_STATUSBAR_HEIGHT_CONSTANT,
+                    zIndex: 100,
+                    backgroundColor: themeColors.background[0] + 'CF',
+                }}
+            />
+            <Header
+                navigation={navigation}
+                title={formatSecondsToTimer(timer)}
+                backgroundColor={themeColors.background[0] + 'CF'}
+                morebutton={
+                    <AntDesign
+                        name="reload1"
+                        size={MEDIUM_ICON_SIZE}
+                        color={themeColors.text[0]}
+                        onPress={() => {}}
+                    />
+                }
+                style={{
+                    marginTop: 0,
+                }}
+            />
+
+            <Animated.View
+                style={[
+                    {
+                        backgroundColor: themeColors.surface[0],
+                        zIndex: 1,
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        alignSelf: 'center',
+
+                        width: 10000,
+                        // height: 1000,
+
+                        transform: [
+                            {
+                                translateX: pan.x,
+                            },
+                            {
+                                translateY: pan.y,
+                            },
+                        ],
+                    },
+                ]}
+                {...panResponder.panHandlers}>
+                {Array(gameData.rows)
+                    .fill(0)
+                    .map((row: any, _) => {
+                        return (
+                            <View
+                                key={_}
+                                style={{
+                                    flexDirection: 'row',
+                                }}>
+                                {Array(gameData.columns)
+                                    .fill(0)
+                                    .map((column: any, _) => {
+                                        return (
+                                            <View
+                                                key={_}
+                                                style={{
+                                                    backgroundColor:
+                                                        themeColors.main[0],
+                                                    width: 32,
+                                                    height: 32,
+                                                    margin: 1,
+                                                    borderRadius: 5,
+                                                }}
+                                            />
+                                        );
+                                    })}
+                            </View>
+                        );
+                    })}
+            </Animated.View>
         </View>
     );
-}
+};
+
+export default GameScreen;
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -23,3 +23,42 @@ export function calculateNumberOfMines(rows: number, columns: number) {
 
     return Math.ceil((15 / 100) * numberOFCells); // default maximum is 15% of number of cells will be mine
 }
+
+/**
+ * two digit number formatter
+ * @param number any number
+ * @returns if number < 10 than `0+${number}` else that number
+ */
+export function formateNumberToHave2Digits(number: number): string {
+    if (number > 99) {
+        return ('0' + number).slice(-3); // for 3 digit numbers
+    }
+    return ('0' + number).slice(-2); // for 1 and 2 digit numbers
+}
+
+/**
+ * seconds to formatted text
+ * @param seconds seconds of any time
+ * @returns formatted text with format HH:MM:ss or MM:ss
+ */
+export function formatSecondsToTimer(seconds: number): string {
+    // if is less than 1 minute
+    if (seconds < 60) {
+        return `00:${formateNumberToHave2Digits(seconds)}`;
+    } else if (seconds >= 60 && seconds < 3600) {
+        // if the seconds make more than 1 minute or so...
+        var min = Math.floor((seconds % 3600) / 60);
+        var sec = Math.floor((seconds % 3600) % 60);
+        return `${formateNumberToHave2Digits(min)}:${formateNumberToHave2Digits(
+            sec,
+        )}`;
+    }
+
+    // if the seconds make more than 1 hours or so...
+    var hour = Math.floor(seconds / 3600);
+    var min = Math.floor((seconds % 3600) / 60);
+    var sec = Math.floor((seconds % 3600) % 60);
+    return `${formateNumberToHave2Digits(hour)}:${formateNumberToHave2Digits(
+        min,
+    )}:${formateNumberToHave2Digits(seconds)}`;
+}
